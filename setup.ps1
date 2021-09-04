47f1898bf095a4e3f9604108f8ac1aad5442e56b
Kenny Yeo <kenny@yeoyou.net>
2021-09-04 21:11:23 +1000

    fix: getting git version

--- setup.ps1
+++ setup.ps1
@@ -35,11 +35,10 @@ $installed_terminal_version = (Get-AppxPackage -Name *WindowsTerminal).Version
 $terminal_url = "https://api.github.com/repos/microsoft/terminal/releases/latest"
 $terminal_asset = Invoke-RestMethod -Method Get -Uri $terminal_url | % assets | where name -like "*msixbundle"
 $terminal_installer = "$env:temp\$($terminal_asset.name)"
+Write-Host $terminal_asset.name
 if ($terminal_asset.name -match $installed_terminal_version) {
-  Write-Host $terminal_asset.name already installed
+  Write-Host already installed
 } else {
-  Write-Host  Windows Terminald
-
   # download installer unless exists
   if (Test-Path($terminal_installer)) {
     Write-Host Found $terminal_installer, skip downloading
@@ -47,7 +46,6 @@ if ($terminal_asset.name -match $installed_terminal_version) {
     Write-Host Downloading $terminal_asset.browser_download_url
     Invoke-WebRequest -UseBasicParsing -Uri $terminal_asset.browser_download_url -OutFile $terminal_installer
   }
-  
   # Install terminal
   Write-Host Installing $terminal_asset.name
   Try {
@@ -61,11 +59,9 @@ if ($terminal_asset.name -match $installed_terminal_version) {
 ######################
 # Install vscode
 Write-Host ---------------------------------------
-try {
+Try {
   $installed_vscode_version = code --version| select-object -First 1
-} catch {
-  Write-Host $_
-}
+} catch {}
 #Write-Host VS Code version: $installed_vscode_version
 
 $vscode_url = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
@@ -77,11 +73,10 @@ $vscode_installer_version = echo $vscode_installer_filename| %{$_.split('-')[-1]
 #Write-Host $vscode_installer_filename, $vscode_installer_version
 
 $vscode_installer = "$env:temp\$($vscode_installer_filename)"
-if ($vscode_installer_version -match $installed_vscode_version) {
-  Write-Host VS Code $vscode_installer_version already installed
+Write-Host VS Code $vscode_installer_version
+if ($installed_vscode_version -And $vscode_installer_version -match $installed_vscode_version) {
+  Write-Host already installed
 } else {
-  Write-Host  VS Code
-
   # download installer unless exists
   if (Test-Path($vscode_installer)) {
     Write-Host Found $vscode_installer, skip downloading
@@ -89,7 +84,6 @@ if ($vscode_installer_version -match $installed_vscode_version) {
     Write-Host Downloading $vscode_installer_url
     Invoke-WebRequest -UseBasicParsing -Uri $vscode_installer_url -OutFile $vscode_installer
   }
-
   # Install vs code
   Write-Host Installing $vscode_installer_filename
   Try {
@@ -100,33 +94,33 @@ if ($vscode_installer_version -match $installed_vscode_version) {
   Write-Host Installed VS Code.
 }
 
-
 ######################
 # run installer if git-bash not found
 Write-Host ---------------------------------------
-$git_install_inf = "$PSScriptRoot\git.inf"
-$git_path = Get-Content $git_install_inf | Select-String "Dir" | Out-String | %{$_.trim()} | % { $_.Substring(4) }
-$git_bash = "$git_path\git-bash.exe"
-$install_args = "/SP- /SILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
-if (Test-Path($git_bash)) {
-  Write-Host Found Git $git_bash
+Try {
+  $installed_git_version = git --version | %{$_.split(' ')[-1]} | %{$_.SubString(0, $_.IndexOf('.windows'))}
+} catch {}
+$git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
+$git_asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
+$git_installer = "$env:temp\$($git_asset.name)"
+
+Write-Host $git_asset.name
+if ($installed_git_version -And $git_asset.name -match $installed_git_version) {
+  Write-Host already installed
 } Else {
-  Write-Host  Git
-  # get latest download url for git-for-windows 64-bit exe
-  $git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
-  $asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
-  
   # download installer unless exists
-  $installer = "$env:temp\$($asset.name)"
+  $installer = "$env:temp\$($git_asset.name)"
   if (Test-Path($installer)) {
     Write-Host Found $installer, skip downloading
   } Else {
-    Invoke-WebRequest -UseBasicParsing -Uri $asset.browser_download_url -OutFile $installer
+    Invoke-WebRequest -UseBasicParsing -Uri $git_asset.browser_download_url -OutFile $installer
   }
-
-  Write-Host Start installing $installer, $install_args
+  # install git
+  $git_install_inf = "$PSScriptRoot\git.inf"
+  $install_args = "/SP- /SILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
+  Write-Host Installing $installer, $install_args
   Start-Process -FilePath $installer -ArgumentList $install_args -Wait
-  Write-Host Done
+  Write-Host Installed Git.
 }
 
 ######################
@@ -134,29 +128,22 @@ if (Test-Path($git_bash)) {
 Write-Host ---------------------------------------
 $vbox_path = "$Env:Programfiles\Oracle\VirtualBox"
 $vbox_manage = "$vbox_path\VBoxManage"
-try {
+Try {
   $installed_vbox_version = (& $vbox_manage --version)
-} catch {
-  Write-Host $_
-}
+} catch {}
 #Write-Host VBox version: $installed_vbox_version
-
 $vbox_url = "https://www.virtualbox.org/wiki/Downloads"
 $vbox_link = (Invoke-WebRequest -UseBasicParsing -Uri $vbox_url).Links | Where-Object {$_.href -like "*Win.exe"}
-
 $vbox_installer_url = [System.Uri]$vbox_link.href
-
 #Write-Host VBox installer url: $vbox_installer_url
 $vbox_installer_filename = $vbox_installer_url.Segments[-1]
 $vbox_installer_version = echo $vbox_installer_filename | %{$_.SubString($_.IndexOf('-')+1)} | %{$_.SubString(0, $_.IndexOf('-Win'))}
-
 $vbox_installer = "$env:temp\$($vbox_installer_filename)"
 #Write-Host $vbox_installer_version, $installed_vbox_version
-if ($installed_vbox_version -match $vbox_installer_version.split('-')[1] ) {
-  Write-Host VBox $vbox_installer_version already installed
+Write-Host VirtualBox $vbox_installer_version
+if ($installed_vbox_version -And $installed_vbox_version -match $vbox_installer_version.split('-')[1] ) {
+  Write-Host already installed
 } else {
-  Write-Host  VBox
-
   # download installer unless exists
   if (Test-Path($vbox_installer)) {
     Write-Host Found $vbox_installer, skip downloading
@@ -164,7 +151,6 @@ if ($installed_vbox_version -match $vbox_installer_version.split('-')[1] ) {
     Write-Host Downloading $vbox_installer_url
     Invoke-WebRequest -UseBasicParsing -Uri $vbox_installer_url -OutFile $vbox_installer
   }
-
   # Install vbox
   Write-Host Installing $vbox_installer_filename
   Try {
@@ -172,17 +158,15 @@ if ($installed_vbox_version -match $vbox_installer_version.split('-')[1] ) {
   } catch {
     Write-Host $_
   }
-  Write-Host Installed VBox.
+  Write-Host Installed VirtualBox.
 }
 
 ######################
 # Install vagrant
 Write-Host ---------------------------------------
-try {
-  $installed_vagrant_version = vagrant --version|%{$_.split(' ')[1]}
-} catch {
-  Write-Host $_
-}
+Try {
+  $installed_vagrant_version = vagrant --version | %{$_.split(' ')[1]}
+} catch {}
 #Write-Host Vagrant version: $installed_vagrant_version
 $vagrant_url = "https://www.vagrantup.com/downloads"
 $vagrant_link = (Invoke-WebRequest -UseBasicParsing -Uri $vagrant_url).Links | Where-Object {$_.href -like "*64.msi"}
@@ -190,11 +174,10 @@ $vagrant_installer_url = [System.Uri]$vagrant_link.href
 $vagrant_installer_filename = $vagrant_installer_url.Segments[-1]
 $vagrant_installer_version = echo $vagrant_installer_filename | %{$_.Split('_')[1]}
 $vagrant_installer = "$env:temp\$($vagrant_installer_filename)"
-if ($installed_vagrant_version -match $vagrant_installer_version ) {
-  Write-Host Vagrant $vagrant_installer_version already installed
+Write-Host Vagrant $vagrant_installer_version
+if ($installed_vagrant_version -And $installed_vagrant_version -match $vagrant_installer_version ) {
+  Write-Host already installed
 } else {
-  Write-Host  Vagrant
-
   # download installer unless exists
   if (Test-Path($vagrant_installer)) {
     Write-Host Found $vagrant_installer, skip downloading
