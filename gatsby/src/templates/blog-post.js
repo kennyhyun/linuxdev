afc887040ef24b487bb573aee1c74080e8e78118
Kenny Yeo <kenny@yeoyou.net>
2021-09-26 01:40:15 +0000

    feat: add gatsby docs

---
+++ gatsby/src/templates/blog-post.js
@@ -0,0 +1,128 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import { DiscussionEmbed } from "disqus-react"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogPostTemplate = ({ data, location }) => {
+  const {
+    markdownRemark: post,
+    previous,
+    next,
+    site: {
+      siteMetadata: {
+        title: siteTitle = `Title`,
+        social: { disqusShortName } = {},
+      } = {},
+    },
+  } = data
+  const disqusConfig = {
+    shortname: disqusShortName,
+    config: { identifier: post.fields.slug, title: post.frontmatter.title },
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          {/*<h1 itemProp="headline">{post.frontmatter.title}</h1>*/}
+          <p>{post.frontmatter.date}</p>
+        </header>
+        <section
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          itemProp="articleBody"
+        />
+        <hr />
+        <footer>
+          <Bio />
+        </footer>
+        <DiscussionEmbed {...disqusConfig} />
+      </article>
+      <hr />
+      <nav className="blog-post-nav">
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  )
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug(
+    $id: String!
+    $previousPostId: String
+    $nextPostId: String
+  ) {
+    site {
+      siteMetadata {
+        title
+        social {
+          disqusShortName
+        }
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+      }
+    }
+    previous: markdownRemark(id: { eq: $previousPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+    next: markdownRemark(id: { eq: $nextPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+  }
+`
