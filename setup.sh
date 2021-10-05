abfb192c4edabd9ae2dc11d545d0bc4bb8b0a374
Kenny Yeo <kenny@yeoyou.net>
2021-10-05 12:16:33 +0000

    feat(setup): add options for mac

--- setup.sh
+++ setup.sh
@@ -1,7 +1,43 @@
 #!/usr/bin/env bash
+for param in "$@"
+do
+  if [ "$param" == "--no-devtools" ] ; then
+    no_devtools=1
+  fi
+  if [ "$param" == "--no-xcode" ] ; then
+    no_xcode=1
+  fi
+  if [ "$param" == "--no-vscode" ] ; then
+    no_vscode=1
+  fi
+  if [ "$param" == "--no-gnused" ] ; then
+    no_gnused=1
+  fi
+  if [ "$param" == "--no-git" ] ; then
+    no_git=1
+  fi
+  if [ "$param" == "--no-iterm2" ] ; then
+    no_iterm2=1
+  fi
+  if [ "$param" == "--no-virtualbox" ] ; then
+    no_virtualbox=1
+  fi
+  if [ "$param" == "--no-vagrant" ] ; then
+    no_vagrant=1
+  fi
+done
+
+if [ $no_devtools ]; then
+    no_xcode=1
+    no_vscode=1
+    no_gnused=1
+    no_git=1
+    no_iterm2=1
+fi
 
 if [[ $(uname -s) != 'Darwin' ]]; then
-  echo This is only for Mac. Please use Porwershell and `setup.ps1`
+  echo This is only for Mac. Please use Porwershell and \`setup.ps1\`
+  exit 1
 fi
 
 echo "=======================================
@@ -12,7 +48,7 @@ git, item2, vscode, virtualbox, vagrant
 echo -n "> Press enter to install or ^C to stop"
 read input
 
-if [[ $(xcode-select -p 1>/dev/null;echo $?) == "0" ]]; then
+if [[ "$no_xcode" || $(xcode-select -p 1>/dev/null;echo $?) == "0" ]]; then
   echo Skip installing xcode-select
 else
   xcode-select --install
@@ -26,20 +62,32 @@ else
 fi
 
 # git
+if [ -z "$no_git" ]; then
 brew install git
+fi
 
 # gnu-sed
+if [ -z "$no_gnused" ]; then
 brew install gnu-sed
+fi
 
 # iterm2
+if [ -z "$no_iterm2" ]; then
 brew install --cask iterm2
+fi
 
 # vscode
+if [ -z "$no_vscode" ]; then
 brew install --cask visual-studio-code
+fi
 
 # virtualbox
+if [ -z "$no_virtualbox" ]; then
 brew install --cask virtualbox
+fi
 
 # vagrant
+if [ -z "$no_vagrant" ]; then
 brew install --cask vagrant
+fi
 
